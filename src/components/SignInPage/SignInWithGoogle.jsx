0f8609b81130bdfca720a098f2f14f3b81ce7046
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-10-05 18:40:04 +0200

    Separate google sign-in/up into its own component

---
+++ src/components/SignInPage/SignInWithGoogle.jsx
@@ -0,0 +1,42 @@
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+
+const SignInWithGoogle = () => {
+  const navigate = useNavigate();
+
+  const clientId =
+    "385042624628-i84c14io7kvle9smpehrgukrdg21qefk.apps.googleusercontent.com";
+
+  const handleLoginSuccess = async (credentialResponse) => {
+    const token = credentialResponse.credential;
+    try {
+      const res = await fetch("http://localhost:3000/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        sessionStorage.setItem("token", data.jwt);
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin
+        onSuccess={handleLoginSuccess}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+    </GoogleOAuthProvider>
+  );
+};
+
+export default SignInWithGoogle;
