28154c5ffa93b340ff6eadfe2658693b04ed9f8d
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-10-18 17:31:28 +0200

    Display basic version of profile section

--- src/components/HomePage/Profile/Profile.jsx
+++ src/components/HomePage/Profile/Profile.jsx
@@ -5,14 +5,34 @@ import { Link } from "react-router-dom";
 const Profile = ({ profile }) => {
   return (
     <div className="profile">
-      <h3 className="profile-header">Profile</h3>
-      <img src={profile.picture} alt="" />
-      <div className="full-name-edit">
-        <div className="full-name">{profile.name}</div>
-        <Link>Edit</Link>
+      <div className="profile-section">
+        <div className="header-close">
+          <h3 className="profile-header">Profile</h3>
+          <button className="close-btn">╳</button>
+        </div>
+        <img src={profile.picture} alt="" />
+        <div className="header-edit">
+          <div className="full-name">{profile.name}</div>
+          <Link>Edit</Link>
+        </div>
       </div>
-      <div>
-        <span>Contact Information</span>
+
+      <hr />
+
+      <div className="profile-section">
+        <div className="header-edit">
+          <span className="profile-section-header">Contact Information</span>
+          <Link>Edit</Link>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="profile-section">
+        <div className="header-edit">
+          <span className="profile-section-header">About me</span>
+          <Link>Edit</Link>
+        </div>
       </div>
     </div>
   );
